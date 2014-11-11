2115fd4ce93a94cefa8bd5c5b23d73e641429ea4
Inooid <justdrawcats@gmail.com>
2014-11-11 12:01:58 +0100

    Run 'annotate' for epic schemes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: activities
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  goal_id    :integer
+#  name       :string(255)
+#  start_date :datetime
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Activity < ActiveRecord::Base
 end
