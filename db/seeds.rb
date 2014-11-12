2b87db381954978caefd5972e7b4c76158c8ddef
Sander Tuin <github@sandert.me>
2014-11-12 19:10:20 +0100

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Competence.create([
+  { name: 'Research & Innovation'},
+  { name: 'Concepting'},
+  { name: 'Multimedia Design'},
+  { name: 'Multimedia Produciton'},
+  { name: 'Quality Monitoring & Implementation'},
+  { name: 'Multidisciplinary Teamwork'},
+  { name: 'Communication'},
+  { name: 'Project Management'},
+  { name: 'Sharing'},
+  { name: 'Growth & Reflection'},
+])
+
+10.times do |i|
+  u = User.create({ email: "user-#{i}@exmaple.com" })
+  10.times do |j|
+    c = Competence.find(j+1)
+    g = Goal.create({ name: "learning goal - #{j}", user: u, competence: c })
+
+    10.times do |k|
+      a = Activity.create({ name: "activity - #{k}", goal: g })
+    end
+  end
+end
