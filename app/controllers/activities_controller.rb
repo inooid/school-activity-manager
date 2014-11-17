08bdca4213d720832d21c7c0a8e77f7d6b160f7e
Sander Tuin <github@sandert.me>
2014-11-17 21:42:32 +0100

    order by start_date desc

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
   def index
-    @activities = Activity.all
+    @activities = Activity.all.order(start_date: :desc)
     @activity = Activity.new
   end
 
